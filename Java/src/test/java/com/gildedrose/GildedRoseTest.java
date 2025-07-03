799143610c0d68463699bb37388706e13c0fdaa3
jonghun <qor6028@naver.com>
2025-07-03 10:42:35 +0900

    test : Sulfuras 아이템에 대한 테스트 케이스 추가

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -35,4 +35,24 @@ class GildedRoseTest {
             () -> assertEquals(1, app.items[1].quality)
         );
     }
+
+    @DisplayName("Sulfuras 아이템은 하루가 지날때마다 가치가 1씩 증가한다.")
+    @Test
+    void updateQualityWhenSulfurasItem() {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertAll(
+            () -> assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name),
+            () -> assertEquals(0, app.items[0].sellIn),
+            () -> assertEquals(80, app.items[0].quality),
+            () -> assertEquals("Sulfuras, Hand of Ragnaros", app.items[1].name),
+            () -> assertEquals(-1, app.items[1].sellIn),
+            () -> assertEquals(80, app.items[1].quality)
+        );
+    }
 }
