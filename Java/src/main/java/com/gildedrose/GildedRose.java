f69ec2e1a6b6f0ca45eb882a6d51e2f1939c8e0e
jonghun <qor6028@naver.com>
2025-07-06 14:09:25 +0900

    refactor : Item 클래스 변경 금지 요구사항에 맞춰 메서드 이동

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
 
+    public static final int MINIMUM = 0;
+    public static final int MAXIMUM = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -10,7 +13,102 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            item.updateQualityBySellIn();
+            updateQualityBySellIn(item);
         }
+    }
+
+    public void updateQualityBySellIn(Item item) {
+        updateQualityByItemType(item);
+        decreaseSellInExceptSulfuras(item);
+        updateQualityWhenNotRemainSellIn(item);
+    }
+
+    private void updateQualityByItemType(Item item) {
+        if (isNormalItem(item.name)) {
+            decreaseQuality(item);
+        } else {
+            increaseQuality(item);
+        }
+    }
+
+    private boolean isNormalItem(String name) {
+        return !isAgedBrie(name) && !isBackstagePasses(name) && !isSulfuras(name);
+    }
+
+    private void decreaseSellInExceptSulfuras(Item item) {
+        if (isSulfuras(item.name)) {
+            return;
+        }
+
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void updateQualityWhenNotRemainSellIn(Item item) {
+        if (hasRemainingSellIn(item.sellIn) || isSulfuras(item.name)) {
+            return;
+        }
+
+        if (isAgedBrie(item.name)) {
+            increaseQualityBy(1, item);
+        } else if (isBackstagePasses(item.name)) {
+            item.quality = MINIMUM;
+        } else if (isDecreasable(item.quality)) {
+            decreaseQuality(item);
+        }
+    }
+
+    private boolean hasRemainingSellIn(int sellIn) {
+        return sellIn >= MINIMUM;
+    }
+
+    private void increaseQuality(Item item) {
+        if (isBackstagePasses(item.name)) {
+            increaseBackstagePassesQuality(item);
+            return;
+        }
+
+        increaseQualityBy(1, item);
+    }
+
+    private void increaseBackstagePassesQuality(Item item) {
+        if (item.sellIn < 6) {
+            increaseQualityBy(3, item);
+        } else if (item.sellIn < 11) {
+            increaseQualityBy(2, item);
+        } else {
+            increaseQualityBy(1, item);
+        }
+    }
+
+    private void increaseQualityBy(int qualityToAdd, Item item) {
+        if (isIncreasable(item.quality, qualityToAdd)) {
+            item.quality = item.quality + qualityToAdd;
+        }
+    }
+
+    private boolean isIncreasable(int quality, int qualityToAdd) {
+        return quality + qualityToAdd <= MAXIMUM;
+    }
+
+    private void decreaseQuality(Item item) {
+        if (isDecreasable(item.quality)) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private boolean isDecreasable(int quality) {
+        return quality > MINIMUM;
+    }
+
+    private boolean isSulfuras(String name) {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstagePasses(String name) {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie(String name) {
+        return name.equals("Aged Brie");
     }
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,9 +2,6 @@ package com.gildedrose;
 
 public class Item {
 
-    public static final int MINIMUM = 0;
-    public static final int MAXIMUM = 50;
-
     public String name;
 
     public int sellIn;
@@ -20,100 +17,5 @@ public class Item {
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
-
-    public void updateQualityBySellIn() {
-        updateQualityByItemType();
-        decreaseSellInExceptSulfuras();
-        updateQualityWhenNotRemainSellIn();
-    }
-
-    private void updateQualityByItemType() {
-        if (isNormalItem()) {
-            decreaseQuality();
-        } else {
-            increaseQuality();
-        }
-    }
-
-    private boolean isNormalItem() {
-        return !isAgedBrie() && !isBackstagePasses() && !isSulfuras();
-    }
-
-    private void decreaseSellInExceptSulfuras() {
-        if (isSulfuras()) {
-            return;
-        }
-
-        sellIn = sellIn - 1;
-    }
-
-    private void updateQualityWhenNotRemainSellIn() {
-        if (hasRemainingSellIn() || isSulfuras()) {
-            return;
-        }
-
-        if (isAgedBrie()) {
-            increaseQualityBy(1);
-        } else if (isBackstagePasses()) {
-            quality = MINIMUM;
-        } else if (isDecreasable()) {
-            decreaseQuality();
-        }
-    }
-
-    private boolean hasRemainingSellIn() {
-        return sellIn >= MINIMUM;
-    }
-
-    private void increaseQuality() {
-        if (isBackstagePasses()) {
-            increaseBackstagePassesQuality();
-            return;
-        }
-
-        increaseQualityBy(1);
-    }
-
-    private void increaseBackstagePassesQuality() {
-        if (sellIn < 6) {
-            increaseQualityBy(3);
-        } else if (sellIn < 11) {
-            increaseQualityBy(2);
-        } else {
-            increaseQualityBy(1);
-        }
-    }
-
-    private void increaseQualityBy(int qualityToAdd) {
-        if (isIncreasable(qualityToAdd)) {
-            quality = quality + qualityToAdd;
-        }
-    }
-
-    private boolean isIncreasable(int qualityToAdd) {
-        return quality + qualityToAdd <= MAXIMUM;
-    }
-
-    private void decreaseQuality() {
-        if (isDecreasable()) {
-            quality = quality - 1;
-        }
-    }
-
-    private boolean isDecreasable() {
-        return quality > MINIMUM;
-    }
-
-    private boolean isSulfuras() {
-        return name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePasses() {
-        return name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isAgedBrie() {
-        return name.equals("Aged Brie");
     }
 }
