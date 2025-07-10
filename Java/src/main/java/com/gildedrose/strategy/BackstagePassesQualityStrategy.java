a3029ecaa4fc451a15d67a2fb4fd5951a070dfcc
jonghunbaek <qor6028@naver.com>
2025-07-10 19:25:34 +0900

    refactor : 조건식 메서드 추출

--- Java/src/main/java/com/gildedrose/strategy/BackstagePassesQualityStrategy.java
+++ Java/src/main/java/com/gildedrose/strategy/BackstagePassesQualityStrategy.java
@@ -6,15 +6,27 @@ public class BackstagePassesQualityStrategy extends QualityStrategy {
 
     @Override
     protected void updateQualityByItemType(Item item) {
-        if (item.sellIn <= MINIMUM) {
+        if (isSellInOver(item)) {
             item.quality = MINIMUM;
-        } else if (item.sellIn < 6) {
+        } else if (isSellInLessThanSixDays(item)) {
             increaseQualityBy(3, item);
-        } else if (item.sellIn < 11) {
+        } else if (isSellInLessThanElevenDays(item)) {
             increaseQualityBy(2, item);
         } else {
             increaseQualityBy(1, item);
         }
+    }
+
+    private boolean isSellInOver(Item item) {
+        return item.sellIn <= MINIMUM;
+    }
+
+    private boolean isSellInLessThanSixDays(Item item) {
+        return item.sellIn < 6;
+    }
+
+    private boolean isSellInLessThanElevenDays(Item item) {
+        return item.sellIn < 11;
     }
 
     @Override
