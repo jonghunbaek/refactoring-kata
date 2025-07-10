e57f4976692f919cf5b028d7953f21fd0a2436ac
jonghunbaek <qor6028@naver.com>
2025-07-10 19:09:20 +0900

    refactor : 변수 인라인

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import com.gildedrose.strategy.QualityStrategy;
 import com.gildedrose.strategy.QualityStrategyFactory;
 
 class GildedRose {
@@ -13,8 +12,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            QualityStrategy strategy = QualityStrategyFactory.createByName(item.name);
-            strategy.updateQualityByItemType(item);
+            QualityStrategyFactory.createByName(item.name)
+                .updateQualityByItemType(item);
         }
     }
 
