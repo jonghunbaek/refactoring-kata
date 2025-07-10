10e7e3be1326e03552d970f795919ca80e0147ac
jonghunbaek <qor6028@naver.com>
2025-07-10 11:54:29 +0900

    test : 누락된 테스트 케이스 추가
    - 일반 아이템은 판매 기간이 남지 않는 경우 quality가 2배로 감소한다.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @DisplayName("일부 아이템을 제외한 모든 아이템은 하루가 지날때마다 판매기간, 가치가 1씩 감소한다.")
+    @DisplayName("일반 아이템은 하루가 지날때마다 판매기간, 가치가 1씩 감소한다.")
     @Test
     void updateQualityWhenNormalItem() {
         Item[] items = new Item[] {new Item("+5 Dexterity Vest", 10, 20)};
@@ -19,6 +19,28 @@ class GildedRoseTest {
             () -> assertEquals("+5 Dexterity Vest", app.items[0].name),
             () -> assertEquals(9, app.items[0].sellIn),
             () -> assertEquals(19, app.items[0].quality)
+        );
+    }
+
+    @DisplayName("일반 아이템은 판매일수가 0미만이면 Quality의 값은 2배로 떨어진다.")
+    @Test
+    void updateQualityWhenNotRemainSellIn() {
+        Item[] items = new Item[] {new Item("+5 Dexterity Vest", 0, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertAll(
+            () -> assertEquals("+5 Dexterity Vest", app.items[0].name),
+            () -> assertEquals(-1, app.items[0].sellIn),
+            () -> assertEquals(18, app.items[0].quality)
+        );
+
+        app.updateQuality();
+
+        assertAll(
+            () -> assertEquals("+5 Dexterity Vest", app.items[0].name),
+            () -> assertEquals(-2, app.items[0].sellIn),
+            () -> assertEquals(16, app.items[0].quality)
         );
     }
 
