ef34d76de1244bf16f53866035053cf7861c6828
jonghunbaek <qor6028@naver.com>
2025-07-10 18:37:27 +0900

    refactor : quality 값을 갱신하는 일부 공통 로직 추상클래스로 복사

--- Java/src/main/java/com/gildedrose/strategy/QualityStrategy.java
+++ Java/src/main/java/com/gildedrose/strategy/QualityStrategy.java
@@ -4,5 +4,32 @@ import com.gildedrose.Item;
 
 public abstract class QualityStrategy {
 
+    public static final int MINIMUM = 0;
+    public static final int MAXIMUM = 50;
+
     public abstract void updateQualityByItemType(Item item);
+
+    protected void increaseQualityBy(int qualityToAdd, Item item) {
+        if (isIncreasable(item.quality, qualityToAdd)) {
+            item.quality = item.quality + qualityToAdd;
+        } else {
+            item.quality = MAXIMUM;
+        }
+    }
+
+    protected boolean isIncreasable(int quality, int qualityToAdd) {
+        return quality + qualityToAdd <= MAXIMUM;
+    }
+
+    protected void decreaseQualityBy(int qualityToSubtract, Item item) {
+        if (isDecreasable(item.quality, qualityToSubtract)) {
+            item.quality = item.quality - qualityToSubtract;
+        } else {
+            item.quality = MINIMUM;
+        }
+    }
+
+    protected boolean isDecreasable(int quality, int qualityToSubtract) {
+        return quality - qualityToSubtract >= MINIMUM;
+    }
 }
